ff4bc3bdaec07231882e9419a7807982bcf6a444
Felipe Sasaki <yuiti_fys@hotmail.com>
2023-07-18 18:34:35 -0300

    My first commit

---
+++ git.py
@@ -0,0 +1,26 @@
+# Dinâmica Git - Webby Internet
+# Regra 1 = Cada integrante deve declarar uma variável com seu nome, e 
+# Regra 2 = A vaviável declarada deve conter um valor que contribua para o resultado final
+
+megazord = ''
+
+# git clone = Clonar um repositório
+# git checkout =
+# git fetch =
+# git branch =
+# git status =
+# git add . = Adicionar os arquivos que serão commitados
+# git commit -m =  
+# git push =  
+
+
+caio = 'Três pratos de '
+sasaki = 'trigo para três '
+pedrinho = 'tigres tristes'
+
+megazord = caio + sasaki + pedrinho
+
+
+## Resultado esperado: 
+# Somos responsáveis por conectar pessoas ao que elas buscam!
+print(megazord)
--- main.py
+++
@@ -1,1 +0,0 @@
-print("Meu primeiro commit")
